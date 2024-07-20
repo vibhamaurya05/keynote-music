2803e7e0eef52ffdd858274203f589a2f6161dd0
vibhamaurya05 <vibhamaurya5sep@gmail.com>
2024-07-20 17:10:19 +0530

    fix: All courses

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import MusicSchoolTestimonials from "@/components/TestimonialCards";
 import FeaturedCourses from "@/components/ui/FeaturedCourses";
 import UpcomingWebinars from "@/components/ui/UpcomingWebinars";
 import WhyChooseUs from "@/components/ui/WhyChooseUs";
-
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -14,6 +15,8 @@ export default function Home() {
         <WhyChooseUs/>
         <MusicSchoolTestimonials/>
         <UpcomingWebinars/>
+        <Instructors/>
+        <Footer/>
     </main>    
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,7 +20,7 @@ function Navbar({ className }: { className?: string }) {
           ></MenuItem>
           <MenuItem setActive={setActive} active={active} item="Our courses">
             <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/courses">Web Development</HoveredLink>
+            <HoveredLink href="/courses">All Courses</HoveredLink>
             <HoveredLink href="/courses">Bsic Music Theory</HoveredLink>
             <HoveredLink href="/courses">Advance composition</HoveredLink>
             <HoveredLink href="/courses">Song Writing</HoveredLink>
